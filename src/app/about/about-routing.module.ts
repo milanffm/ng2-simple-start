049db92804a6d66bf21811f1de9985bff95efa91
Milan Peters <m.peters@graphicon.de>
2017-10-10 22:06:58 +0200

    remove old css and delete default path by about

--- src/app/about/about-routing.module.ts
+++ src/app/about/about-routing.module.ts
@@ -4,7 +4,6 @@ import { Routes, RouterModule } from '@angular/router';
 import { AboutComponent } from './about.component';
 
 const routes: Routes = [
-  { path: '', redirectTo: '/about', pathMatch: 'full' },
   { path: 'about', component: AboutComponent }
 ];
 
