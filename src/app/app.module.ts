c9ee1f366e3b1a9c69a65899d3876c7bc457624c
Milan Peters <m.peters@graphicon.de>
2017-10-10 22:31:04 +0200

    add header component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,12 @@ import { AppRoutingModule } from './app-routing.module';
 import { HomeModule } from './home/home.module';
 import { AboutModule } from './about/about.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    [AppComponent]
+      AppComponent,
+      HeaderComponent
   ],
   imports: [
     BrowserModule,
