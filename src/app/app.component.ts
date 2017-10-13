859d7c7d753e2951714d712c9dcc2d8a43273fe0
Milan Peters <m.peters@graphicon.de>
2017-10-13 22:21:55 +0200

    setTitle on app start

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import { Title } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'app';
+export class AppComponent implements OnInit{
+  constructor( private titleService: Title) {}
+  ngOnInit() {
+    this.titleService.setTitle('simple App');
+  }
 }
