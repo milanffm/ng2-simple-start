47738363cf09fd8b49854a08d9a9d765d1870bb1
Milan Peters <m.peters@graphicon.de>
2017-10-30 15:01:49 +0100

    add interface to posts get observable

--- src/app/services/post.service.ts
+++ src/app/services/post.service.ts
@@ -1,17 +1,17 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import 'rxjs/add/operator/map';
-import { Observable } from 'rxjs/Observable';
+interface Posts {
+  posts: [object];
+}
 
 @Injectable()
 export class PostService {
 
   postsUrl = 'http://localhost:3000/api/post/list';
-
   constructor(private http: HttpClient) {}
 
-  getPosts(): Observable<any> {
-    return this.http.get(this.postsUrl);
+  getPosts() {
+    return this.http.get<Posts>(this.postsUrl);
   }
 
 }
