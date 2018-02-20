f838803b457e79743cb663203032108aa357a0e4
Milan Peters <m.peters@graphicon.de>
2018-02-20 08:55:56 +0100

    add icon font and scss files

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -4,7 +4,7 @@ import { I18nService } from '@app/core/services/i18n.service';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
 
