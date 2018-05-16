fcacfa7fb0e84cedc253e6e8088476c3771044bc
Milan Peters <m.peters@graphicon.de>
2018-05-16 11:20:42 +0200

    remove rxjs-compat and add new version ngx-translate for angular 6

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,24 +1,18 @@
-import 'rxjs/add/observable/merge';
-import 'rxjs/add/operator/filter';
-import 'rxjs/add/operator/first';
-import 'rxjs/add/operator/map';
-import 'rxjs/add/operator/mergeMap';
+import {Component, OnInit} from '@angular/core';
+import {Title, Meta} from '@angular/platform-browser';
+import {TranslateService} from '@ngx-translate/core';
+import {merge} from 'rxjs';
+import {filter, map, mergeMap} from 'rxjs/operators';
+import {Router, NavigationEnd, ActivatedRoute} from '@angular/router';
 
-import { Component, OnInit } from '@angular/core';
-import { Title, Meta } from '@angular/platform-browser';
-import { TranslateService } from '@ngx-translate/core';
-import { Observable } from 'rxjs/Observable';
-import { Router, NavigationEnd, ActivatedRoute } from '@angular/router';
-
-import { environment } from '@env/environment';
-import { I18nService } from '@app/core/services/i18n.service';
-
+import {environment} from '@env/environment';
+import {I18nService} from '@app/core/services/i18n.service';
 
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
     constructor(private router: Router,
@@ -26,44 +20,46 @@ export class AppComponent implements OnInit {
                 private titleService: Title,
                 private translateService: TranslateService,
                 private i18nService: I18nService,
-                private metaService: Meta) { }
+                private metaService: Meta) {
+    }
 
 
-  ngOnInit() {
+    ngOnInit() {
 
-      console.log('init');
+        console.log('init');
 
-      // Setup translations
-      this.i18nService.init(environment.defaultLanguage, environment.supportedLanguages);
+        // Setup translations
+        this.i18nService.init(environment.defaultLanguage, environment.supportedLanguages);
 
-      const onNavigationEnd = this.router.events.filter(event => event instanceof NavigationEnd);
+        const onNavigationEnd = this.router.events.pipe(filter(event => event instanceof NavigationEnd));
 
-      // Change page title and MetaDescription on navigation or language change, based on route data
-      Observable.merge(this.translateService.onLangChange, onNavigationEnd)
-          .map(() => {
-              let route = this.activatedRoute;
-              while (route.firstChild) {
-                  route = route.firstChild;
-              }
-              return route;
-          })
-          .filter(route => route.outlet === 'primary')
-          .mergeMap(route => route.data)
-          .subscribe(event => {
-              const title = event['title'];
-              if (title) {
-                  this.titleService.setTitle(this.translateService.instant(title));
-              }
-              if (event['metaDescriptionTranslationPath']) {
-                  this.translateService.get(event['metaDescriptionTranslationPath'])
-                      .subscribe((res: string) => {
-                          const tag = {name: 'description', content: res};
-                          const attributeSelector = 'name="description"';
-                          this.metaService.removeTag(attributeSelector);
-                          this.metaService.addTag(tag, false);
-                      });
-              }
-          });
-
-  }
+        // Change page title and MetaDescription on navigation or language change, based on route data
+        merge(this.translateService.onLangChange, onNavigationEnd)
+            .pipe(
+                map(() => {
+                    let route = this.activatedRoute;
+                    while (route.firstChild) {
+                        route = route.firstChild;
+                    }
+                    return route;
+                }),
+                filter(route => route.outlet === 'primary'),
+                mergeMap(route => route.data)
+            )
+            .subscribe(event => {
+                const title = event['title'];
+                if (title) {
+                    this.titleService.setTitle(this.translateService.instant(title));
+                }
+                if (event['metaDescriptionTranslationPath']) {
+                    this.translateService.get(event['metaDescriptionTranslationPath'])
+                        .subscribe((res: string) => {
+                            const tag = {name: 'description', content: res};
+                            const attributeSelector = 'name="description"';
+                            this.metaService.removeTag(attributeSelector);
+                            this.metaService.addTag(tag, false);
+                        });
+                }
+            });
+    }
 }
