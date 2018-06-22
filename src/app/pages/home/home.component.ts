ebab3f48b51b646ae9c645d76186f8370eac5a10
Milan Peters <m.peters@graphicon.de>
2018-06-22 09:03:39 +0200

    change slider

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,6 +10,11 @@ export class HomeComponent implements OnInit {
 	constructor() {
 	}
 
+	NextPhotoInterval = 3000;
+	noPause = true;
+	noLoopSlides = false;
+	setIndex = 1;
+
 	data: object;
 
 	ngOnInit() {
