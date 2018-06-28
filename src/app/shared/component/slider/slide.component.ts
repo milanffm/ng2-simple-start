98561b70097e6653327c14f020fe11755b1fa351
Milan Peters <m.peters@graphicon.de>
2018-06-28 14:56:12 +0200

    add fancy animations to slide compontent

--- src/app/shared/component/slider/slide.component.ts
+++ src/app/shared/component/slider/slide.component.ts
@@ -1,28 +1,35 @@
 import {Component, OnInit, OnDestroy, Input, HostBinding, OnChanges} from '@angular/core';
 
 import {CarouselComponent, Direction} from './carousel.component';
-import {animate, style, transition, trigger} from '@angular/animations';
+import {animate, query, stagger, style, transition, trigger, group} from '@angular/animations';
 
 @Component({
 	selector: 'app-slide',
 	template: `
-        <div [class.active]="active" *ngIf="active" [ngClass]="'item ' + customClass" [@slideBgAnimation] >
+        <div [class.active]="active" *ngIf="active" [ngClass]="'item ' + customClass" [@image] >
             <ng-content></ng-content>
         </div>
     `,
 	styleUrls: ['./slide.compontent.scss'],
 	animations: [
-		trigger(
-			'slideBgAnimation', [
-				transition(':enter', [
-					style({opacity: 0}),
-					animate(1500, style({opacity: 1}))
+		trigger('image', [
+			transition(':enter', [
+				query('*', [
+					style({  opacity: 0 }),
 				]),
-				transition(':leave', [
-					animate(1500, style({opacity: 0}))
+				query('*', [
+					style({ transform: 'translateY(100px)' }),
+					stagger(100, [
+						animate('1200ms cubic-bezier(0.35, 0, 0.25, 1)', style('*'))
+					])
+				], { delay: 500 })
+			]),
+			transition(':leave', [
+				query('*', [
+					animate('500ms', style({ transform: 'translateY(50px)', opacity: 0 }))
 				])
-			],
-		),
+			])
+		])
 	]
 })
 export class SlideComponent implements OnInit, OnDestroy, OnChanges {
